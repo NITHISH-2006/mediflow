5e496ed850a5336a1c3aeb64c0612e266e122768
NITHISH <itsmenithish.official@gmail.com>
2026-08-02 16:21:39 +0530

    chore: support DATABASE_URL/INTERNAL_DB_URL and untrack .env

--- backend/config/database.go
+++ backend/config/database.go
@@ -16,14 +16,21 @@ var DB *gorm.DB
 
 // ConnectDB initialises the PostgreSQL connection and auto-migrates all models.
 func ConnectDB() {
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-	)
+	var dsn string
+	if url := os.Getenv("DATABASE_URL"); url != "" {
+		dsn = url
+	} else if url := os.Getenv("INTERNAL_DB_URL"); url != "" {
+		dsn = url
+	} else {
+		dsn = fmt.Sprintf(
+			"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+			os.Getenv("DB_PORT"),
+		)
+	}
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
